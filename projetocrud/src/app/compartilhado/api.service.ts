6f098a18eb6da2cf13c0f4465e9fa03b6b83d61d
Markcell Schwenck <markcell.1@gmail.com>
2022-04-12 13:49:58 -0300

    crud

--- projetocrud/src/app/compartilhado/api.service.ts
+++ projetocrud/src/app/compartilhado/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators'
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,9 +10,27 @@ export class ApiService {
 
   constructor(private http : HttpClient) { }
 
-  postProdutos(data : any){
+  postCadastro(data : any){
     return this.http.post<any>("http://localhost:3000/posts", data)
-    .pipe( map ())
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+
+  }
+
+  getCadastro(data : any){
+    return this.http.post<any>("http://localhost:3000/posts", data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+
+  }
+
+  updateCadastro(data : any){
+    return this.http.post<any>("http://localhost:3000/posts", data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
 
   }
 }
